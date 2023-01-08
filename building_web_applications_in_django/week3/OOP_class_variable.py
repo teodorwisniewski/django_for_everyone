a82d393fb9d55de4e04335764eb8d8fc0b335b2d
Teodor Wisniewski <teodorwisniewski@gmail.com>
2023-01-08 13:04:37 +0100

    we change instance varaible, and class variable is intact

--- building_web_applications_in_django/week3/OOP_class_variable.py
+++ building_web_applications_in_django/week3/OOP_class_variable.py
@@ -1,37 +1,6 @@
 
 
 
-class Test:
-    """
-    ex:
-        t1 = Test("Teodor")
-        t1.party()
-        t1.party()
-        t1.party()
-        t1.party()
-
-        t2 = Test("Mat")
-        t2.party()
-
-    Output:
-        Hey Teodor, x is equal to 1
-        Hey Teodor, x is equal to 2
-        Hey Teodor, x is equal to 3
-        Hey Teodor, x is equal to 4
-        Hey Mat, x is equal to 5
-
-
-    """
-    x: int = 0
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def party(self):
-        Test.x += 1
-        print(f"Hey {self.name}, x is equal to {self.x}")
-
-
 # class Test:
 #     """
 #     ex:
@@ -45,12 +14,11 @@ class Test:
 #         t2.party()
 #
 #     Output:
-#         Hey Teodor, x is equal to 0
-#         Hey Teodor, x is equal to 0
-#         Hey Teodor, x is equal to 0
-#         Hey Teodor, x is equal to 0
-#         Hey Mat, x is equal to 0
-#
+#         Hey Teodor, x is equal to 1
+#         Hey Teodor, x is equal to 2
+#         Hey Teodor, x is equal to 3
+#         Hey Teodor, x is equal to 4
+#         Hey Mat, x is equal to 5
 #
 #
 #     """
@@ -60,8 +28,38 @@ class Test:
 #         self.name = name
 #
 #     def party(self):
-#         self.x += 1
-#         print(f"Hey {self.name}, x is equal to {Test.x}")
+#         Test.x += 1
+#         print(f"Hey {self.name}, x is equal to {self.x}")
+
+
+class Test:
+    """
+    ex:
+        t1 = Test("Teodor")
+        t1.party()
+        t1.party()
+        t1.party()
+        t1.party()
+
+        t2 = Test("Mat")
+        t2.party()
+
+    Output:
+        Hey Teodor, x is equal to 0
+        Hey Teodor, x is equal to 0
+        Hey Teodor, x is equal to 0
+        Hey Teodor, x is equal to 0
+        Hey Mat, x is equal to 0
+    """
+    x: int = 0
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def party(self):
+        self.x += 1
+        print(f"Hey {self.name}, x is equal to {Test.x}")
+
 
 # class Test:
 #     x: int = 0
